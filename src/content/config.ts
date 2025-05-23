3d8b6b573155d1bf2dd7365ee61c72bde8a51179
hanbings <hanbings@hanbings.io>
2025-05-24 05:40:21 +0800

    ⬆️ Feat: Updated to a new version of astro.

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,19 @@ const postsCollection = defineCollection({
     })
 })
 
+const eventsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.string(),
+        draft: z.boolean().optional(),
+        image: z.string().optional(),
+        author: z.string().optional()
+    })
+})
+
 export const collections = {
     posts: postsCollection,
+    events: eventsCollection,
 }
