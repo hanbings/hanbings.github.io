6c6e179371a8d57e34a668d38b467548a429bade
hanbings <hanbings@hanbings.io>
2026-07-19 16:58:59 +0800

    ✨ Feat: Add Rough Notation MDX effects.

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,13 +1,25 @@
+import Box from './rough-notation/Box.astro'
+import Bracket from './rough-notation/Bracket.astro'
+import Circle from './rough-notation/Circle.astro'
+import CrossedOff from './rough-notation/CrossedOff.astro'
+import Highlight from './rough-notation/Highlight.astro'
+import StrikeThrough from './rough-notation/StrikeThrough.astro'
+import Underline from './rough-notation/Underline.astro'
 import Piano from './piano/Piano.astro'
 import Sidenote from './sidenote/Sidenote.astro'
 import SidenoteText from './sidenote/SidenoteText.astro'
 import Term from './term/Term.astro'
-import Underline from './underline/Underline.astro'
 
 export const mdxComponents = {
+  Box,
+  Bracket,
+  Circle,
+  CrossedOff,
+  Highlight,
   Piano,
   Sidenote,
   SidenoteText,
+  StrikeThrough,
   Term,
   Underline,
 }
---
+++ src/components/mdx/rough-notation/types.ts
@@ -0,0 +1,21 @@
+export type RoughNotationType =
+  'underline' | 'box' | 'circle' | 'highlight' | 'strike-through' | 'crossed-off' | 'bracket'
+
+export type RoughNotationPadding = number | [number, number] | [number, number, number, number]
+export type RoughNotationBracket = 'left' | 'right' | 'top' | 'bottom'
+
+export interface RoughNotationProps {
+  color?: string
+  strokeWidth?: number
+  iterations?: number
+  padding?: RoughNotationPadding
+  animationDuration?: number
+  animate?: boolean
+  multiline?: boolean
+  rtl?: boolean
+  class?: string
+}
+
+export interface BracketProps extends RoughNotationProps {
+  brackets?: RoughNotationBracket | RoughNotationBracket[]
+}
