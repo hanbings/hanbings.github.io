5b22cbd04e3b1ce5c3bfe954085f86bb32036153
hanbings <hanbings@hanbings.io>
2026-07-21 13:58:46 +0800

    🔨 Refactor: Extract the fullscreen vertex shader.

---
+++ src/components/mdx/shader-box/fullscreen-triangle.vert.glsl
@@ -0,0 +1,12 @@
+#version 300 es
+precision highp float;
+
+const vec2 positions[3] = vec2[](
+  vec2(-1.0, -1.0),
+  vec2(3.0, -1.0),
+  vec2(-1.0, 3.0)
+);
+
+void main() {
+  gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+}
--- src/components/mdx/shader-box/shader-box.ts
+++ src/components/mdx/shader-box/shader-box.ts
@@ -1,16 +1,4 @@
-const vertexShaderSource = `#version 300 es
-precision highp float;
-
-const vec2 positions[3] = vec2[](
-  vec2(-1.0, -1.0),
-  vec2(3.0, -1.0),
-  vec2(-1.0, 3.0)
-);
-
-void main() {
-  gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
-}
-`
+import vertexShaderSource from './fullscreen-triangle.vert.glsl?raw'
 
 class MdxShaderBox extends HTMLElement {
   private canvas?: HTMLCanvasElement
